4265e49173010aed47aaae3765f78f5ffc5e91fd
George Ornbo <george@shapeshed.com>
2023-07-11 10:53:27 +0100

    Transfer WIP

--- src/msg.rs
+++ src/msg.rs
@@ -1,10 +1,17 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Uint128;
 
 #[cw_serde]
 pub struct InstantiateMsg {}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    Transfer {
+        address: String,
+        tokens_to_send: Uint128,
+        denom: String,
+    },
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
